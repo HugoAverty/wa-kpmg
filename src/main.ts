985a757564f24dca241eed78fb78c26fa754ab4b
Hugo Averty <hugoaverty@gmail.com>
2022-07-04 15:21:40 +0200

    Fixing CI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-//import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
 
 export {};
