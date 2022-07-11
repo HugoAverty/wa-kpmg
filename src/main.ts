719c1d509809d63fccf812b93b8d27baae025b3d
Hugo Averty <hugoaverty@gmail.com>
2022-07-11 17:46:07 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    let helloWorldPopup;
+let currentPopup: any = undefined;
 // Open the popup when we enter a given zone
 WA.room.onEnterLayer("myZone").subscribe(() => {
     helloWorldPopup = WA.ui.openPopup("popupRectangle", "Bienvenue sur les bureaux virtuels de Digital Audit Leaders, vous pouvez les utiliser durant toute la durée du programme pour vous retrouver en équipes, partager des moments conviviaux ou encore avancer sur vos projets en mode collaboratifs ! N'hésitez pas à explorer l'espace", [{
